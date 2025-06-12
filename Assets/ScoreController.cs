92b54395671b7bb6376b1d39d7b3a094d135f5a4
RENTAL-03\student <https://github.com/ryo-cpu/Pendilum.git>
2025-06-12 11:24:45 +0900

    スコア実装

--- Assets/ScoreController.cs
+++ Assets/ScoreController.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class ScoreController:MonoBehaviour
 {
+    public TextMeshProUGUI ScoreText;
     int Score;
 
     void Start()
     {
+        UpdateScoreText();
         Score = 0;
     }
     public void Add(int score)
@@ -15,5 +19,9 @@ public class ScoreController:MonoBehaviour
     public int GetScore()
     {
         return this.Score;
+    }
+    void UpdateScoreText()
+    {
+        ScoreText.text = "Score: " + Score.ToString();
     }
 }
